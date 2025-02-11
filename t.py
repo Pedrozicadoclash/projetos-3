9f0532b4e8b2a9c6f6f2e0da28baa58f7aaca900
Pedrozicadoclash <pedroguilhermeferraridegoes@gmail.com>
2025-02-11 18:30:27 -0300

    Initial commit

---
+++ t.py
@@ -0,0 +1,7 @@
+num1 = int(input("Escreva um numero"))
+num2 = int(input("Escreva outro numero"))
+sinal = input("Digite um sinal")
+if (sinal=='+'):
+    resultado = num1+num2
+    print("O resultado é: ",resultado)
+    
