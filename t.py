a34fdaf4d3cdf15a989222d6152606f866222892
Pedrozicadoclash <pedroguilhermeferraridegoes@gmail.com>
2025-02-12 21:21:05 -0300

    site

--- t.py
+++
@@ -1,7 +0,0 @@
-num1 = int(input("Escreva um numero"))
-num2 = int(input("Escreva outro numero"))
-sinal = input("Digite um sinal")
-if (sinal=='+'):
-    resultado = num1+num2
-    print("O resultado é: ",resultado)
-    
